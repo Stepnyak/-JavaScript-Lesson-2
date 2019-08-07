22d5bb39e1aaf97524ef779efb535e575ff31d2f
Vladimir <kursinov@mail.ru>
2019-08-08 00:29:33 +0300

    Третий коммит правильный

--- dz2.js
+++ dz2.js
@@ -14,11 +14,12 @@ for (let i=0;i<5;i++)
 {
 let a=prompt("Какой товар будем продавать?");
 
-if ((typeof(a))==='string' && (typeof(a)) === null && a!='' && a.length <20) {
+if ((typeof(a))==='string' && (typeof(a)) === null && a!=' ' && a.length <20) {
 console.log("все верно");
-mainList.ShopGoods[i]=a;	}
-prompt("Верно");
-}
+mainList.ShopGoods[i]=a;
+} else 
+	{mainList.ShopGoods[i]=a;}
+} 
 alert(mainList.budjet / 30);
 
 

b6c234d55e1bee25485165c72379ec8a75f9cf61
Vladimir <kursinov@mail.ru>
2019-08-08 00:39:07 +0300

    4 коммит заключительный

--- dz2.js
+++ dz2.js
@@ -16,12 +16,11 @@ let a=prompt("Какой товар будем продавать?");
 
 if ((typeof(a))==='string' && (typeof(a)) === null && a!=' ' && a.length <20) {
 console.log("все верно");
-mainList.ShopGoods[i]=a;
-} else 
-	{mainList.ShopGoods[i]=a;}
+mainList.ShopGoods[i]=a;	} else {mainList.ShopGoods[i]=a;}
+//prompt("Верно");
 } 
 alert(mainList.budjet / 30);
 
 
-console.log(mainList.ShopGoods[3]);
+console.log(mainList.ShopGoods);
 console.log(mainList);

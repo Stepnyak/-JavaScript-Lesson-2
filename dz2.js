78c15fa068f476ec343757a405424e25ddd160a7
Vladimir <kursinov@mail.ru>
2019-08-08 08:28:50 +0300

    Правильно выполнено  ДЗ 2 ВСЕ РАБОТАЕТ

--- dz2.js
+++ dz2.js
@@ -14,11 +14,12 @@ for (let i=0;i<5;i++)
 {
 let a=prompt("Какой товар будем продавать?");
 
-if ((typeof(a))==='string' && (typeof(a)) === null && a!=' ' && a.length <20) {
-console.log("все верно");
-mainList.ShopGoods[i]=a;	} else {mainList.ShopGoods[i]=a;}
-//prompt("Верно");
-} 
+if ((typeof(a))==='string' && (typeof(a)) != null && a!=' ' && a.length <20) {
+	console.log("все верно");
+	mainList.ShopGoods[i]=a; } 
+else 
+	{i=i-1;}
+ 
 alert(mainList.budjet / 30);
 
 
